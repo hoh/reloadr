70988673d2fa38624f59a02a77c2295a754e6579
Hugo Herter <git@hugoherter.com>
2015-11-30 18:11:23 +0100

    Add support for reload functions

--- reloadr.py
+++ reloadr.py
@@ -5,24 +5,39 @@
 import inspect
 import redbaron
 import threading
+import types
 from time import sleep
 
 
-def get_new_source(target):
+def get_new_class_source(target):
     filepath = inspect.getsourcefile(target)
     red = redbaron.RedBaron(open(filepath).read())
     return red.find('class', target.__name__).dumps()
 
 
 def reload_class(target):
-    source = get_new_source(target)
+    source = get_new_class_source(target)
     module = inspect.getmodule(target)
     locals_ = {}
     exec(source, module.__dict__, locals_)
     return locals_[target.__name__]._target
 
 
-class Reloadr:
+def get_new_function_source(target):
+    filepath = inspect.getsourcefile(target)
+    red = redbaron.RedBaron(open(filepath).read())
+    return red.find('def', target.__name__).dumps()
+
+
+def reload_function(target):
+    source = get_new_function_source(target)
+    module = inspect.getmodule(target)
+    locals_ = {}
+    exec(source, module.__dict__, locals_)
+    return locals_[target.__name__]._target
+
+
+class ClassReloadr:
     def __init__(self, target):
         self._target = target
         self._instance = None
@@ -52,4 +67,37 @@ class Reloadr:
         print(thread)
 
 
-reloadr = Reloadr
+class FuncReloadr:
+    def __init__(self, target):
+        self._target = target
+        self._instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self._instance is None:
+            self._init_args = args
+            self._init_kwargs = kwargs
+            self._instance = self._target.__call__(*args, **kwargs)
+            return self._instance
+        else:
+            return self._instance.__call__(*args, **kwargs)
+
+    def _reload(self):
+        self._instance = reload_function(self._target)
+
+    def _autoreload(self, interval=1):
+        while True:
+            self._reload()
+            sleep(interval)
+
+    def _start_autoreload(self, interval=1):
+        thread = threading.Thread(target=self._autoreload)
+        print(thread)
+        thread.start()
+        print(thread)
+
+
+def reloadr(target):
+    if isinstance(target, types.FunctionType):
+        return FuncReloadr(target)
+    else:
+        return ClassReloadr(target)
