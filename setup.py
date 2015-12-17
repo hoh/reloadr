eeb2c4c4a9f2bc636fd9820dc84ce3b128460099
Hugo Herter <git@hugoherter.com>
2015-12-17 19:15:15 +0100

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,38 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup
+# This file is part of Supernova.
+#
+# Copyright (C) 2014 Railnova (https://www.railnova.eu)
 
-import reloadr
+import os
 
-setup(
-    name='reloadr',
-    version=reloadr.__version__,
-    description='Fast and simple hot code reloader tool.',
-    long_description=reloadr.__doc__,
-    author=reloadr.__author__,
-    author_email='pypi@hugoherter.com',
-    url='https://github.com/hoh/Reloadr/',
-    py_modules=['reloadr'],
-    scripts=['reloadr.py'],
-    license='Apache',
-    platforms='any',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Debuggers',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3.4',
-        ],
-    )
+from setuptools import setup, find_packages
+from reloadr import __version__
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(name='Reloadr',
+      version=__version__,
+      description='Hot code reloading tool for Python',
+      long_description=long_description,
+      author='Hugo Herter',
+      author_email='git@hugoherter.com',
+      url='https://github.com/hoh/reloadr',
+      packages=find_packages(),
+      include_package_data=True,
+      data_files=[],
+      install_requires=[
+          'redbaron',
+          ],
+      license='LGPLv3',
+      keywords="reload hot code reloading",
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Programming Language :: Python :: 3.4',
+                   'Operating System :: POSIX :: Linux',
+                   'Intended Audience :: Developers',
+                   ],
+      )
